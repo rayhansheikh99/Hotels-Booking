a48a5f7247f739200aeb30b70df0cb7c2a474bdc
rayhansheikh99 <rsheikh201031@msce.uiu.ac.bd>
2022-05-20 01:25:07 +0600

    header added

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -1,13 +1,36 @@
 import React from 'react';
-import "./header.css"
-
+import "./header.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBed, faCar, faPlane, faTaxi } from '@fortawesome/free-solid-svg-icons'
 const Header = () => {
     return (
         <div className='header'>
-            <div className="header-list">
-                <div className="header-list-item">
-
+            <div className='header-container'> 
+                <div className="header-list">
+                    <div className="header-list-item active">
+                    <FontAwesomeIcon icon={faBed} />
+                    <span>Stays</span>
+                    </div>
+                    <div className="header-list-item">
+                    <FontAwesomeIcon icon={faPlane} />
+                    <span>Flights</span>
+                    </div>
+                    <div className="header-list-item">
+                    <FontAwesomeIcon icon={faCar} />
+                    <span>Car Rentals</span>
+                    </div>
+                    <div className="header-list-item">
+                    <FontAwesomeIcon icon={faBed} />
+                    <span>Attractions</span>
+                    </div>
+                    <div className="header-list-item">
+                    <FontAwesomeIcon icon={faTaxi} />
+                    <span>Airport Taxis</span>
+                    </div>
                 </div>
+                <h1 className='header-title'>A lifetime of discounts? It's Genius.</h1>
+                <p className='header-desc'>Get rewarded for your travels – unlock instant savings of 10% or more with a free Booking.com account</p>
+                <button className='header-btn'>Sign In / Register</button>
             </div>
         </div>
     );
